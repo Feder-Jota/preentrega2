b07ccb7c761ff744d66d940a55167dcebf70293f
Federico Juarez <112991094+Feder-Jota@users.noreply.github.com>
2022-12-15 16:14:57 -0300

    actualizado y funcionando
    en el console.log("El total de Puntos del gurpo F es "+total)

--- scrip.js
+++ scrip.js
@@ -1,20 +1,17 @@
 
-function max(arrayGrupoF)
 const arrayGrupoF = [
    { nombre: "Marruecos", Pos: 1, Ganados: 2, empatado: 1, perdidos: 0, puntos: 7 },
    { nombre: "Croacia", Pos: 2, Ganados: 1, empatado: 2, perdidos: 0, puntos: 5 },
    { nombre: "Bélgica", Pos: 3, Ganados: 1, empatado: 1, perdidos: 1, puntos: 4 },
-   { nombre: "Canadá", Pos: 4, Ganados: 0, empatado: 0, perdidos: 2, puntos: 0 },
+   { nombre: "Canadá", Pos: 4, Ganados: 0, empatado: 0, perdidos: 2, puntos: 0 }
 ]
 
-function max(arrayGroupF){
-   const mayor= arrayGroupF.reduce((inicial,actual)=> (inicial.puntos > actual.puntos) ? inicial: actual).puntos
-   return mayor
-   
+function max(arrayGrupoF){
+   const mayor = arrayGrupoF.reduce((inicial,actual)=> (inicial.puntos > actual.puntos) ? inicial: actual).puntos
+   return mayor   
 }
-reduce()
 
-let resultado=max(arrayGroupF)
+let resultado=max(arrayGrupoF)
 
 let total = 0
 
@@ -23,4 +20,5 @@ let total = 0
    total= (total+ arrayGrupoF[i].puntos)
  }
 
-console.log("El total de Puntos del gurpo F es ", total)
+
+console.log("El total de Puntos del gurpo F es "+total)
